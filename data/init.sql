830f200b304a931127459fca72ed30dcb6401f3a
Eric Schmidt <eric.schmidt1350@gmail.com>
2021-03-08 07:30:11 -0500

    Added config.php, install.php, updated init.sql

--- data/init.sql
+++ data/init.sql
@@ -1,13 +1,10 @@
-CREATE DATABASE IF NOT EXISTS sports_app;
-
-    use sports_app;
-
+CREATE DATABASE IF NOT EXISTS hockeyapp;
+    USE hockeyapp;
     CREATE TABLE Teams (
         id INT(11) UNSIGNED AUTO_INCREMENT UNIQUE NOT NULL,
-        name VARCHAR(40) NOT NULL,
+        teamname VARCHAR(40) NOT NULL,
         PRIMARY KEY (id)
     );
-
     CREATE TABLE Players (
         id INT(11) UNSIGNED AUTO_INCREMENT NOT NULL,
         teamID INT(11) UNSIGNED NOT NULL,
@@ -18,7 +15,6 @@ CREATE DATABASE IF NOT EXISTS sports_app;
         PRIMARY KEY (id),
         FOREIGN KEY (teamID) REFERENCES Teams(id)
     );
-
     CREATE TABLE Stats (
         playerID INT(11) UNSIGNED UNIQUE NOT NULL,
         gamesplayed INT(3) DEFAULT 0,
@@ -30,36 +26,28 @@ CREATE DATABASE IF NOT EXISTS sports_app;
         faceoffslost INT(4) DEFAULT 0,
         faceoffpercentage DECIMAL(2,1) DEFAULT 0.0
     );
-
     ALTER TABLE Stats
     ADD CONSTRAINT FK_player_stats FOREIGN KEY (playerID)
         REFERENCES Players(id);
-
-
     /*Test Data*/
-    INSERT INTO teams(name)
+    INSERT INTO teams(teamname)
     VALUES("Boston Bruins");
-
     INSERT INTO players(teamID,firstname,lastname,age,position)
     VALUES(1,"Bob","Smith",30,"Center");
     INSERT INTO stats(playerID)
     VALUES(1);
-
     INSERT INTO players(teamID,firstname,lastname,age,position)
     VALUES(1,"Phil","Duke",25,"Left Wing");
     INSERT INTO stats(playerID)
     VALUES(2);
-
     INSERT INTO players(teamID,firstname,lastname,age,position)
     VALUES(1,"Steve","Dome",28,"Goalie");
     INSERT INTO stats(playerID)
     VALUES(3);
-
     INSERT INTO players(teamID,firstname,lastname,age,position)
     VALUES(1,"Jake","Tucker",19,"Right Wing");
     INSERT INTO stats(playerID)
     VALUES(4);
-
     INSERT INTO players(teamID,firstname,lastname,age,position)
     VALUES(1,"Mark","White",31,"Defense");
     INSERT INTO stats(playerID)
--- install.php
+++ install.php
@@ -1,5 +1,25 @@
 <?php
-$servername = "localhost";
-$user = "root"
-$password = "root"
+
+    require "config.php";
+
+    $connection = new mysqli($host, $username, $password);
+
+    if($connection->connect_error){
+        die("Connection failed: " . $connection->connect_error);
+    }
+    else{
+        echo "Connection Success<br />";
+    }
+
+    $sql = file_get_contents("data/init.sql");
+    $connection->query($sql);
+
+    if($connection->multi_query($sql) === TRUE){
+        echo "Database and tables created";
+    }
+    else{
+        echo "Database Error: " . $connection->error;
+    }
+    
+    $connection->close();
 ?>
